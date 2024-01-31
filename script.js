3702e5aaecf895c57b38a877c1274034287d8298
cys2110 <csheridan2110@gmail.com>
2024-01-31 16:57:12 -0500

    Update

---
+++ script.js
@@ -0,0 +1,18 @@
+const body = document.querySelector('p')
+
+const validScrabbleWord = async() => {
+    const response = await fetch("/1986/12/15.json")
+    const list = await response.json()
+    const answers = list.answers.across
+    const clues = list.clues.across
+    const combined = answers.map((answer, clue) => ({[answer]: clues[clue]}))
+    const stringed = JSON.stringify(combined)
+    body.innerHTML += stringed
+    const downAnswers = list.answers.down
+    const downClues = list.clues.down
+    const downCombined = downAnswers.map((answer, clue) => ({[answer]: downClues[clue]}))
+    const downStringed = JSON.stringify(downCombined)
+    body.innerHTML += downStringed
+  }
+
+validScrabbleWord()
