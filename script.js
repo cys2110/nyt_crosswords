cdbcb6cf613b5446b1f5f05ae21b7eee068aa505
cys2110 <csheridan2110@gmail.com>
2024-02-08 08:44:11 -0500

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const body = document.querySelector('p')
 
 const validScrabbleWord = async() => {
-    const response = await fetch("/1977/08/14.json")
+    const response = await fetch("/2016/05/28.json")
     const list = await response.json()
     const answers = list.answers.across
     const clues = list.clues.across
